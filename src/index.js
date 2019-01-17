d84963f71ebc420df9430c03a18a3b36808a6351
xinpeicheng <xinpeicheng@yeah.net>
2019-01-17 11:52:48 +0800

    增加puppeteer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import '@babel/polyfill'
 import './config/env'
+import {resolve} from 'path'
+import Puppeteer from 'puppeteer'
 (async ()=>{
+    const browser  = await Puppeteer.launch({
+        executablePath:resolve(__dirname,'./../chrome-win/chrome.exe'),
+        headless :false
+    })
+    const Page =await browser.newPage()
+    await Page.goto('https://account.geekbang.org/signin')
+    const html = Page.$('.forget');
+    console.log(html)
     await console.log(process.env.PASSWORD)
 })()
