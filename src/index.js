238996fe8be88f2575d6e084b460b5a62ef45852
xinpeicheng <xinpeicheng@yeah.net>
2019-01-18 10:19:42 +0800

    增加爬取内容句柄

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import {resolve} from 'path'
 import Puppeteer from 'puppeteer'
 import Login from './module/login'
 import GetCatalog from './module/getCatalog'
-// import { setOra } from './util/util'
-// import './config/ora'
+
 (async ()=>{
     const browser  = await Puppeteer.launch({
         executablePath:resolve(__dirname,'./../chrome-win/chrome.exe'),
--- src/module/Crawl.js
+++ src/module/Crawl.js
@@ -2,10 +2,13 @@ import {ACCOUNT,COLUMO_DOMAIN,PASSWORD} from './../config/env'
 
 const Crawl = async (Page,browser,ID)=>{
     const newPage = await browser.newPage();
-    // console.log(`${COLUMO_DOMAIN}/column/${ID}`)
     await newPage.goto(`${COLUMO_DOMAIN}/column/${ID}`);
-    newPage.click('.article-item',{options :{
+    await newPage.click('.article-item',{options :{
         button :'middle'
     }})
+    // 清理数据
+    const $HTML_el = newPage.$('html')
+    const html = $HTML_el.outerHTML()
+
 }
 export default Crawl
