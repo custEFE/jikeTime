391aab8c3f811196d84f2b94520be27bc4747576
xinpeicheng <xinpeicheng@yeah.net>
2019-01-25 18:14:14 +0800

    重构为使用接口请求的方式爬取数据，下一步解决进程问题，理想状态下多进程应该持续爬

--- src/module/getCatalog.js
+++ src/module/getCatalog.js
@@ -4,10 +4,15 @@ import {createdFolder,getCookies} from './../util/util'
 import {resolve} from 'path'
 import {fork} from 'child_process'
 import Rp from 'request-promise-native'
+import Os from 'os'
+
+const cpuNum = Os.cpus().length
+const articlesURL = 'https://time.geekbang.org/serv/v1/column/articles'
 
 const GetCatalog = async (Page,browser) => {
     const cookies = await Page.cookies()
     const cookie = await getCookies(cookies)
+    let childRps = []
     // options
     const options = {
         uri:'https://time.geekbang.org/serv/v1/my/products/all',
@@ -22,28 +27,54 @@ const GetCatalog = async (Page,browser) => {
     if (res.data.length!==0){
         data = res.data[0]
     }
-
-    const childFork = fork('./startCrawling.js',['abc'])
-    childFork.on('message',msg=>{
-        console.log(msg)
+    data.request = options
+    childRps = data.list.map(v=>{
+        const options = {
+            uri:articlesURL,
+            body:{
+                "cid":v.extra.column_id,
+                "size":90,
+                "prev":0,
+                "order":"newest",
+                "sample":true
+            },
+            headers:{
+                Cookie:cookie,
+                Referer:"https://account.geekbang.org/dashboard/buy"
+            },
+            json:true
+        }
+        return Rp(options)
     })
-    // console.log(data)
-    // Page.on('requestfinished',async res=>{
-    //     if (res.url() === `https://time.geekbang.org/serv/v1/my/products/all`){
-    //         const response = await res.response()
-    //         const JSON_DATA = await response.json()
-    //         const curriculums =  JSON_DATA.data[0].list
-    //         const request = response.request()
 
-
-    //         // 列表页的ID /column/132 
-    //         // await createdFolder(resolve(__diranme,'./../../html'))
-    //         console.log(cookie)
-    //         // await Crawl(Page,browser,curriculums[0].extra.column_id)
-    //         // // curriculums.forEach(v => {
-    //         // //     console.log(v.extra.column_id)
-    //         // // });
-    //       }
-    // })
+    const listRes = await Promise.all(childRps)
+    const ids = listRes.map(v=>{
+        const ids = []
+        for (let i=0;i<v.data.list.length;i++){
+            const item = v.data.list[i]
+            ids.push(item.id)
+        }
+        return ids
+    })
+    const childData = {
+        ids:ids[0],
+        headers:options.headers
+    }
+    run(JSON.stringify(childData))
 }
+
+const run = data=>{
+    const childFork = fork(resolve(__dirname,'./startCrawling.js'))
+
+    childFork.on('message',msg=>{
+        if (msg === 'readyStart'){
+            childFork.send(data)
+        }else if (msg === 'close'){
+            childFork.close()
+            console.log('关闭子进程')
+        }
+    })
+    childFork.send('start')
+}
+
 export default GetCatalog
--- src/module/startCrawling.js
+++ src/module/startCrawling.js
@@ -1,5 +1,29 @@
-console.log('child pid: ');
-// process.on('message', function(msg){
-//     console.log('child get message: ' + JSON.stringify(msg));
-// });
-// process.send({key: 'child value'});
+import Rp from 'request-promise-native'
+
+process.on('message', function(msg){
+    if (msg ==='start'){
+        process.send('readyStart')
+    }else {
+        run(msg)
+    }
+});
+// 留个坑，下次解决
+const run = data=>{
+    const parentProcessData = JSON.parse(data)
+    const requestFun = parentProcessData.ids.map(v=>{
+        const options = {
+            uri:'https://time.geekbang.org/serv/v1/article',
+            body:{
+                "id":v,
+                include_neighbors:true
+            },
+            headers:parentProcessData.headers,
+            json:true
+        }
+        return Rp(options)
+    })
+    Promise.all(requestFun).then(res=>{
+        console.log(res)
+        process.send('close')
+    })
+}
